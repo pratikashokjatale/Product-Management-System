8205b5d48f1fc11761b41c97c16bb031f23a4c18
pratikashokjatale <95568174+pratikashokjatale@users.noreply.github.com>
2025-01-21 00:04:41 +0530

    Update CategoryController.java

--- ProductManagement/src/main/java/com/example/demo/controller/CategoryController.java
+++ ProductManagement/src/main/java/com/example/demo/controller/CategoryController.java
@@ -16,27 +16,27 @@ public class CategoryController {
     private CategoryService categoryService;
 
     @GetMapping
-    public Page<Category> getAllCategories(Pageable pageable) {
+    public Page<Category> getAllTheCategories(Pageable pageable) {
         return categoryService.getAllCategories(pageable);
     }
 
     @GetMapping("/{id}")
-    public Category getCategoryById(@PathVariable Long id) {
+    public Category getCategoryUsingId(@PathVariable("id") Long id) {
         return categoryService.getCategoryById(id);
     }	
 
     @PostMapping
-    public Category createCategory(@RequestBody Category category) {
+    public Category createNewCategory(@RequestBody Category category) {
         return categoryService.createCategory(category);
     }
 
     @PutMapping("/{id}")
-    public Category updateCategory(@PathVariable Long id, @RequestBody Category category) {
+    public Category updateExistingCategory(@PathVariable("id") Long id, @RequestBody Category category) {
         return categoryService.updateCategory(id, category);
     }
 
     @DeleteMapping("/{id}")
-    public void deleteCategory(@PathVariable Long id) {
+    public void deleteExistingCategory(@PathVariable("id") Long id) {
         categoryService.deleteCategory(id);
     }
 }
